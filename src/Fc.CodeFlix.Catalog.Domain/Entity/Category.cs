7b062fb54b3bd3ed11dd510a73a927357fcba4cd
pugnatore <paulofmb@hotmail.com>
2024-07-26 10:45:15 +0100

    Add category tests

---
+++ src/Fc.CodeFlix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Openvia">
+//     Copyright (c) Openvia. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Fc.CodeFlix.Catalog.Domain.Entity;
+
+public class Category
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Category(string name, string description, bool isActive = true)
+    {
+        this.Id = Guid.NewGuid();
+        this.Name = name;
+        this.Description = description;
+        this.IsActive = true;
+        this.CreatedAt = DateTime.Now;
+        this.IsActive = isActive;
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.UnitTets/Domain/Entity/Category/CategoryTests.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Openvia">
+//     Copyright (c) Openvia. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace FC.CodeFlix.Catalog.UnitTets.Domain.Entity.Category;
+
+using Fc.CodeFlix.Catalog.Domain.Entity;
+
+public class CategoryTests
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        // Arrange
+        var validData = new
+        {
+            Name = "Category Name",
+            Description = "Category Description",
+        };
+        var dateTimeBefore = DateTime.Now;
+
+        // Act
+        var category = new Category(validData.Name, validData.Description);
+
+        var dateTimeAfter = DateTime.Now;
+
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(Guid.Empty, category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt >= dateTimeBefore && category.CreatedAt <= dateTimeAfter);
+        Assert.True(category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        // Arrange
+        var validData = new
+        {
+            Name = "Category Name",
+            Description = "Category Description",
+        };
+        var dateTimeBefore = DateTime.Now;
+
+        // Act
+        var category = new Category(validData.Name, validData.Description, isActive);
+
+        var dateTimeAfter = DateTime.Now;
+
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(Guid.Empty, category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt >= dateTimeBefore && category.CreatedAt <= dateTimeAfter);
+        Assert.Equal(isActive,category.IsActive);
+    }
+}
